d2fccaee89a0a154c715be09b207ac018e047de1
jarebones <jarebones@live.ca>
2015-03-26 12:43:42 -0400

    Everything
    Website Files

---
+++ README.md
@@ -0,0 +1,15 @@
+# JMeyerOnline Main Site
+
+This project includes all the files necessary to run my main website.
+
+## Creator
+
+Everything in this project was put together by Jared Meyer. Free to use components such as the filterable portfolio were taken from around the web and incorporated. All photography used around site, as well as all featured projects are property of Jared Meyer.
+
+* http://jmeyeronline.com/
+
+Start Bootstrap is based on the [Bootstrap](http://getbootstrap.com/) framework created by [Mark Otto](https://twitter.com/mdo) and [Jacob Thorton](https://twitter.com/fat).
+
+## Copyright
+
+Copyright 2015 Jared Meyer
---
+++ js/custom.js
@@ -0,0 +1,193 @@
+$(document).ready(function(){
+
+    $('.hidden').css('display','none');
+
+    $( "#filter button" ).each(function() {
+
+        $(this).on("click", function(){
+
+            var filter = $(this).attr('class');         
+
+            if ( $(this).attr('class') == 'all' ) {
+                $('.hidden').contents().appendTo('#posts').hide().show('slow');
+                $( "#filter button" ).removeClass('active');
+                $(this).addClass('active');
+                $("#filter button").attr("disabled", false);
+                $(this).attr("disabled", true);
+            } else {
+                $('.post').appendTo('.hidden');
+                $('.hidden').contents().appendTo('#posts').hide().show('slow');
+                $('.post:not(.' + filter + ')').appendTo('.hidden').hide('slow');
+                $( "#filter button" ).removeClass('active');
+                $(this).addClass('active');
+                $("#filter button").attr("disabled", false);
+                $(this).attr("disabled", true);
+            };
+          
+        });
+
+    });
+
+    $(".carousel-inner button").click(function(){
+        toPortfolio();
+    })
+
+
+//Firefox
+$("body").on('DOMMouseScroll', function(e){
+    var $currentSection = $("section:in-viewport").attr("id");
+    if(e.originalEvent.detail > 0) {
+        //scroll down
+        switch ($currentSection){
+            case "mainSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#aboutSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+
+            case "aboutSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#workSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "workSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#contactSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "contactSection":
+                //do nothing
+                break;        
+        }
+    }else {
+        //scroll up
+        switch ($currentSection){
+            case "mainSection":
+                //do nothing
+                break;
+
+            case "aboutSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#mainSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "workSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#aboutSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "contactSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#workSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;        
+        }
+    }
+
+    console.log($currentSection);
+
+    //prevent page fom scrolling
+    return false;
+});
+
+ //IE, Opera, Safari
+ $("body").on('mousewheel', function(e){
+    var $currentSection = $("section:in-viewport").attr("id");
+    
+     if(e.originalEvent.wheelDelta < 0) {
+         //scroll down
+         switch ($currentSection){
+            case "mainSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#aboutSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+
+            case "aboutSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#workSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "workSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#contactSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "contactSection":
+                //do nothing
+                break;        
+        }
+     }else {
+         //scroll up
+         switch ($currentSection){
+            case "mainSection":
+                //do nothing
+                break;
+
+            case "aboutSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#mainSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "workSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#aboutSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;
+            
+            case "contactSection":
+                $('html, body').stop().animate({
+                    scrollTop: $("#workSection").offset().top
+                }, 1500, 'easeInOutExpo');
+                break;        
+        }
+     }
+
+     console.log($currentSection);
+
+     //prevent page fom scrolling
+     return false;
+ });
+
+})
+
+$(window).load(function() {
+ 
+    //setTimeout(function(){
+    	$("#navBar").hide();
+    	$(".nav").css("visibility", "visible");
+    	$(".navbar").css("visibility", "visible");
+        $('body').addClass('loaded');
+        setTimeout(function(){
+        	$("#navBar").fadeIn(1000,function(){
+        		//do nothing
+        	});
+    	}, 1000);
+    //}, 3000);
+ 
+});
+
+function toPortfolio(section) {
+
+    $("#workCarousel").fadeOut(500,function(){
+        $("#workPortfolio").fadeIn(500,function(){
+            //do nothing
+        });
+    });
+}
+
+function toCarousel() {
+
+    $("#workPortfolio").fadeOut(500,function(){
+        $("#workCarousel").fadeIn(500,function(){
+            //do nothing
+        });
+    });
+}
